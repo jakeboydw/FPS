8f5a0ff25c1328a1553914910071f4d8455f1792
jakeboydw <1465766129@qq.com>
2026-03-29 15:31:22 +0800

    AI logic

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,241 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+using System.Linq;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnPoint;
+    public GameObject weaponFlash;
+    public float bloom;
+    public float fireRate;
+    private float lastShotTime = 0f;
+
+    public Material hitMat;
+
+    private Rigidbody rb;
+    private Renderer rend;
+    private Material originalMaterial;
+
+    // AI Settings
+    public int currentPointIndex = 0;
+    public Vector3 currentTarget;
+    public float positionThreshold;
+    public float idleTime = 5f;
+    public float attackDistance = 5f;
+    public float maxVisionDistance = 20f;
+    public float minChasingHealth = 30f;
+
+    public Transform[] patrolPoints;
+    private float idleTimeCounter;
+    private Transform playerTransform;
+    private bool canSeePlayer;
+    private Vector3 lastKnownPlayerPosition;
+
+    private NavMeshAgent agent;
+
+    public enum State { Idle, Patrolling, Chasing, Attacking };
+    private State state = State.Idle;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+        originalMaterial = rend.material;
+
+        agent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.FindWithTag("Player").transform;
+
+        GameObject patrolPointParent = GameObject.FindWithTag("PatrolPoint");
+        patrolPoints = patrolPointParent.GetComponentsInChildren<Transform>().Where(t => t != patrolPointParent.transform).ToArray();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Damage"))
+        {
+            health -= 10;
+
+            if (health <= 0)
+            {
+                Die();
+            }
+            else
+            {
+                StartCoroutine(Blink());
+            }
+        }
+    }
+
+    void Die()
+    {
+        if (!this.enabled) return;
+
+        rb.freezeRotation = false;
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + 5f);
+        this.enabled = false;
+    }
+
+    IEnumerator Blink()
+    {
+        rend.material = hitMat;
+        yield return new WaitForSeconds(0.1f);
+        rend.material = originalMaterial;
+    }
+
+    private void Update()
+    {
+        LookForPlayer();
+
+        switch (state)
+        {
+            case State.Idle:
+                Idle();
+                break;
+            case State.Patrolling:
+                Patrolling();
+                break;
+            case State.Chasing:
+                Chasing();
+                break;
+            case State.Attacking:
+                Attacking();
+                break;
+        }
+
+        rb.linearVelocity = Vector3.zero;
+
+        LookAtPlayer();
+        SetLastKnownPlayerPosition();
+    }
+
+    private void LookForPlayer()
+    {
+        Vector3 directionToPlayer = playerTransform.position - transform.position;
+        if (Physics.Raycast(transform.position, directionToPlayer, out RaycastHit hit, maxVisionDistance))
+        {
+            canSeePlayer = hit.transform == playerTransform;
+            if (canSeePlayer && state != State.Attacking)
+            {
+                state = State.Chasing;
+            }
+        }
+    }
+
+    private void Idle()
+    {
+        agent.ResetPath();
+
+        idleTimeCounter -= Time.deltaTime;
+        if (idleTimeCounter < 0)
+        {
+            state = State.Patrolling;
+            idleTimeCounter = idleTime;
+        }
+    }
+
+    private void Patrolling()
+    {
+        if (Vector3.Distance(currentTarget, transform.position) < positionThreshold)
+        {
+            int chance = Random.Range(0, 100);
+
+            if (chance <= 10)
+            {
+                state = State.Idle;
+                return;
+            }
+
+            currentPointIndex++;
+            currentTarget = patrolPoints[currentPointIndex % patrolPoints.Length].position;
+        }
+        else
+        {
+            agent.SetDestination(currentTarget);
+        }
+    }
+
+    private void Chasing()
+    {
+        idleTimeCounter = idleTime;
+        agent.SetDestination(lastKnownPlayerPosition);
+
+        if (health < minChasingHealth)
+        {
+            state = State.Patrolling;
+        }
+        else if (Vector3.Distance(transform.position, playerTransform.position) <= attackDistance && canSeePlayer)
+        {
+            state = State.Attacking;
+        }
+        else if (Vector3.Distance(transform.position, playerTransform.position) > maxVisionDistance)
+        {
+            state = State.Patrolling;
+        }
+        else if (Vector3.Distance(transform.position, playerTransform.position) < positionThreshold && !canSeePlayer)
+        {
+            state = State.Patrolling;
+        }
+    }
+
+    private void Attacking()
+    {
+        idleTimeCounter = idleTime;
+        agent.ResetPath();
+
+        Shoot();
+
+        if (Vector3.Distance(transform.position, playerTransform.position) > attackDistance || !canSeePlayer)
+        {
+            if (health < minChasingHealth)
+            {
+                state = State.Patrolling; //Cautious
+            }
+            else
+            {
+                state = State.Chasing;
+            }
+        }
+    }
+
+    private void LookAtPlayer()
+    {
+        if (canSeePlayer)
+        {
+            transform.LookAt(new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z));
+        }
+    }
+
+    private void SetLastKnownPlayerPosition()
+    {
+        if (canSeePlayer)
+        {
+            lastKnownPlayerPosition = playerTransform.position;
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Time.time > lastShotTime + fireRate)
+        {
+            Vector3 directionToPlayer = playerTransform.position - transform.position;
+            directionToPlayer.Normalize();
+
+            Quaternion bulletRotation = Quaternion.LookRotation(directionToPlayer);
+
+            float maxInaccuracy = 10f;
+            float currentInaccuracy = bloom * maxInaccuracy;
+            float randomYaw = Random.Range(-currentInaccuracy, currentInaccuracy);
+            float randomPitch = Random.Range(-currentInaccuracy, currentInaccuracy);
+
+            bulletRotation *= Quaternion.Euler(randomPitch, randomYaw + 90, 0f);
+
+            Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletRotation);
+            Instantiate(weaponFlash, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            lastShotTime = Time.time;
+        }
+    }
+}
